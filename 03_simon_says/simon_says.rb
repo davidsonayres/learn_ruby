dca29b4cfab953784cd1358462b31bf29ceed948
Michael <mjames.ayres@gmail.com>
2016-10-26 21:40:47 -0400

    Update repeat method

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -6,10 +6,16 @@ def shout(string)
   return string.capitalize
 end
 
-def repeat(string)
-  return "#{string} #{string}"
-end
+# def repeat(string)
+#   return "#{string} #{string}"
+# end
 
 # def repeat(string)
 #   return string + " " + string
 # end
+
+def repeat(string, num = 2)
+  num.times do
+    return string
+  end
+end
