c34955caf4433a5315b7d86c78353c1ecdd840a2
Michael <mjames.ayres@gmail.com>
2016-10-26 20:38:35 -0400

    Define method ctof

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,5 +1,9 @@
 # 1. One degree fahrenheit is 5/9 of one degree celsius
 # 2. The freezing point of water is 0 degrees celsius but 32 degrees fahrenheit
 def ftoc(temp)
-  return (temp - 32) * 5.0/9.0
+  (temp - 32) * 5.0/9.0
+end
+
+def ctof(temp)
+  temp * 9.0/5.0 + 32
 end
