f6201f5788ebb32b05edbdc9d634a1f97e17f9f4
Michael <mjames.ayres@gmail.com>
2016-10-26 20:36:03 -0400

    Define method ftoc

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,2 +1,5 @@
 # 1. One degree fahrenheit is 5/9 of one degree celsius
 # 2. The freezing point of water is 0 degrees celsius but 32 degrees fahrenheit
+def ftoc(temp)
+  return (temp - 32) * 5.0/9.0
+end
