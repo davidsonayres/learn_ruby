3213bdc14e082c7d8471b4a2605660b93c4e5ae7
Michael <mjames.ayres@gmail.com>
2016-11-02 12:49:05 -0400

    Update start_of_word method

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,3 +1,4 @@
+
 def echo(string)
   string
 end
@@ -16,14 +17,14 @@ end
 
 def repeat(string, num=2)
   num.times do |s|
-    print "#{string} "
+    return "#{string} "
   end
 end
 
 def start_of_word(word, num = 0)
   letters = word.split("")
   l = num - 1
-  letters(l)
+  letters[l]
 end
 
 def first_word(string)
@@ -33,15 +34,11 @@ end
 
 def titleize(string)
   array = string.split(" ")
-  ignore = ["if", "and", "or", "the", "an", "a"]
+  ignore = ["if", "and", "or", "the", "an", "a", "but", "of"]
   array.each do |word|
-    if word == ignore.each
-      word.downcase!
-      unless word = array[0]
-        word.capitalize!
-      end
-    else
-      word.capitalize!
+    case word
+    when word == ignore.each then word.downcase!
+    else word.capitalize!
     end
   end
   title = array.join(" ")
